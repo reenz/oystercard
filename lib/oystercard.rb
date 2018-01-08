1c8f1c730d2ee1d04c66b37e5501d788ac70da83
reena <reena.sanjay.sharma@gmail.com>
2018-01-08 22:13:56 +0000

    Added touch in/out support

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,20 +1,30 @@
 class Oystercard
 attr_reader :balance
+attr_accessor :in_journey
 BALANCE_LIMIT = 90
   def initialize
    @balance = 0
+   @in_journey = false
   end
 
   def top_up(amount)
     raise "Balance limit of #{BALANCE_LIMIT} reached" if amount +balance > BALANCE_LIMIT
     @balance += amount
   end
-<<<<<<< HEAD
-=======
 
   def deduct(amount)
     @balance -= amount
   end
 
->>>>>>> d4f03ea6f26d7a0444a84e1726fd12c47f00605f
+  def in_journey?
+   @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -28,4 +28,26 @@ describe Oystercard do
     end
   end
 
+  describe '#in_journey?' do
+    before do
+      subject.top_up(Oystercard::BALANCE_LIMIT)
+    end
+
+    it 'checks if oystercard is touched in'do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+end
+
+  describe '#touch_out'do
+    it 'checks if oystercard is touched out'do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 end
