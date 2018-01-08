f7de8b10870e8a636a3cf866cde5ba3343522bb4
reena <reena.sanjay.sharma@gmail.com>
2018-01-08 16:29:31 +0000

    added balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 attr_reader :balance
-
+BALANCE_LIMIT = 90
   def initialize
    @balance = 0
   end
 
   def top_up(amount)
+    raise "Balance limit of #{BALANCE_LIMIT} reached" if amount +balance > BALANCE_LIMIT
     @balance += amount
   end
+
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,5 +12,12 @@ describe Oystercard do
     it 'can top up the balance' do
       expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
     end
+
+    it 'raises error when balance excedes balance limit' do
+      balance_limit = Oystercard::BALANCE_LIMIT
+      subject.top_up(balance_limit)
+      error = "Balance limit of #{balance_limit} reached"
+      expect{subject.top_up(1)}.to raise_error error
+    end
   end
 end
